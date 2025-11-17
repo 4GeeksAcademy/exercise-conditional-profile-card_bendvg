bba96696597f3bdf4195ad25d8680f5fde624e5e
Benji <bendvg@gmail.com>
2025-11-17 18:57:02 +0000

    cambios3

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name || "Lucy"} ${variables.lastName || "Boilett"}</h1>
-          <h2>${variables.role || 'Web developer'}</h2>
+          <h1>${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}</h1>
+          <h2>${variables.role || "Web developer"}</h2>
           <h3>${variables.city || "Miami"}, ${variables.country || "USA"} </h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
