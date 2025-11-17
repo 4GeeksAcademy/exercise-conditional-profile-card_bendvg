483c4c7fcd67484e9d95156c80d7efed96f1390b
Benji <bendvg@gmail.com>
2025-11-17 19:23:21 +0000

    cambios4

--- src/js/app.js
+++ src/js/app.js
@@ -37,11 +37,15 @@ function render(variables = {}) {
     "Boilett"}</h1>
           <h2>${variables.role || "Web developer"}</h2>
           <h3>${variables.city || "Miami"}, ${variables.country || "USA"} </h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition || "position-right"}">
+           <li><a href="https://twitter.com/${variables.twitter ||
+             "4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
+           <li><a href="https://github.com/${variables.github ||
+             "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+           <li><a href="https://linkedin.com/in/${variables.linkedin ||
+             "4geeksacademy"}"><i class="fab fa-linkedin"></i></a></li>
+           <li><a href="https://instagram.com/${variables.instagram ||
+             "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
